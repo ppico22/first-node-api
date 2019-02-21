9074db4f39cff3edfb9bc42d77068c409164f4e7
Pedro <pedrop@capitaira.com>
2019-02-20 23:58:48 -0300

    all in index

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 //generate an id that not repeat
 const uuidv4 = require('uuid/v4')
+const _ = require('underscore')
 
 const app = express()
 app.use(bodyParser.json())
@@ -21,7 +22,7 @@ app.route('/productos')
         let nuevoProducto = req.body
         if (!nuevoProducto.moneda || !nuevoProducto.precio || !nuevoProducto.titulo) {
             //Bad request
-            res.status(404).send(`Tu producto debe especificar un titulo, precio y moenda`)
+            res.status(404).send(`Tu producto debe especificar un titulo, precio y moneda`)
             return
         }
 
@@ -30,18 +31,46 @@ app.route('/productos')
         res.status(201).json(nuevoProducto)
     })
 
-app.get('/productos/:id', (req, res) => {
-        for(let producto of productos) {
-            if(producto.id == req.params.id){
-                res.json(producto)
-                return
+app.route('/productos/:id')
+    .get((req, res) => {
+            for(let producto of productos) {
+                if(producto.id == req.params.id){
+                    res.json(producto)
+                    return
+                }
             }
+            //Not found
+            res.status(404).send(`El producto con id ${req.params.id} no existe`)
+        })
+    .put((req, res) => {
+        let id = req.params.id
+        let reemplazoParaProducto = req.body
+
+        if (!reemplazoParaProducto.moneda || !reemplazoParaProducto.precio || !reemplazoParaProducto.titulo) {
+            //Bad request
+            res.status(400).send(`Tu producto debe especificar un titulo, precio y moneda`)
+            return
         }
-        //Not found
-        res.status(404).send(`El producto con id ${req.params.id} no existe`)
+
+        let indice = _.findIndex(productos, producto => producto.id == id)
+        
+        if(indice !== -1){
+            reemplazoParaProducto.id = id
+            productos[indice] = reemplazoParaProducto
+            res.status(200).json(reemplazoParaProducto)
+        }else{
+            res.status(404).send(`El producto con id ${id} no existe`)
+        }
+    }) 
+    .delete((req, res) => {
+        let indiceABorrar = _.findIndex(productos, producto => producto.id == req.params.id)
+        if(indiceABorrar === -1){
+            res.status(404).send(`Producto con id [${req.params.id}] no existe. Nada que borrar.`)
+            return
+        }
+        let borrado = productos.splice(indiceABorrar, 1)
+        res.json(borrado)
     })
-
-
 
 app.get('/', (req, res) => {
     res.send('first api get')
