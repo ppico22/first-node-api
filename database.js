6c00cba7095d7197b0d49f6e3f5d1707116aac92
Pedro <pedrop@capitaira.com>
2019-02-21 00:08:50 -0300

    database module export

---
+++ database.js
@@ -0,0 +1,10 @@
+//Base de datos en memoria
+const productos = [
+    {id:'1234', titulo:'macbook', precio: 1300, moneda:'USD'},
+    {id:'123', titulo:'iphone', precio: 800, moneda:'USD'},
+    {id:'12', titulo:'tazas', precio: 50, moneda:'USD'},
+]
+
+module.exports = {
+    productos
+}
--- index.js
+++ index.js
@@ -3,16 +3,11 @@ const bodyParser = require('body-parser')
 //generate an id that not repeat
 const uuidv4 = require('uuid/v4')
 const _ = require('underscore')
+const productos = require('./database').productos
 
 const app = express()
 app.use(bodyParser.json())
 
-//Base de datos en memoria
-const productos = [
-    {id:'1234', titulo:'macbook', precio: 1300, moneda:'USD'},
-    {id:'123', titulo:'iphone', precio: 800, moneda:'USD'},
-    {id:'12', titulo:'tazas', precio: 50, moneda:'USD'},
-]
 
 app.route('/productos')
     .get((req, res) => {
